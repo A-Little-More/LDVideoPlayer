dbe6ed7fd29aa5441c547926befe8b06e7e0d590
ld <ld123456>
2018-06-26 14:22:45 +0800

    Initial Commit

---
+++ LDVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LDVideoPlayer
+//
+//  Created by lidong on 2018/6/26.
+//  Copyright © 2018年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LDVideoPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LDVideoPlayer
+//
+//  Created by lidong on 2018/6/26.
+//  Copyright © 2018年 macbook. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
